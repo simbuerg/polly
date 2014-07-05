718b42b53730ae77ef687f60544e153d480ac1b6
cib <cib123@googlemail.com>
2014-07-05 11:53:21 +0200

    Created EWPTTableEntry class modeled after the EWPT paper.

--- lib/Analysis/EWPTAliasAnalysis.cpp
+++ lib/Analysis/EWPTAliasAnalysis.cpp
@@ -6,25 +6,75 @@
 
 using namespace llvm;
 
-/**
- * @brief Describes a unique region of memory.
- */
-class EWPTMemoryRegion {
-private:
-    const Value* IdentifiedValue;
+class EWPTConstraint {
 public:
-    EWPTMemoryRegion(const Value& IdentifiedValue)
-        : IdentifiedValue(&IdentifiedValue) {
-        // TODO: add iteration vector to constructor
+    /** The index of the variable this constraint applies to. */
+    const int appliedVariable;
+
+    /** The temporary index that we constrain this variable to.
+    *  Temporary indexes represent either `i` or `i-`.
+    */
+    const int appliedIndex;
+
+    EWPTConstraint(int appliedVariable, int appliedIndex)
+        : appliedVariable(appliedVariable), appliedIndex(appliedIndex) { }
+};
+
+
+class EWPTIndexConstraint {
+public:
+    const bool isPreviousIndex;
+
+    EWPTIndexConstraint(bool isPreviousIndex) : isPreviousIndex(isPreviousIndex) {
+
     }
 };
 
-class EWPTConstraint {
+class EWPTTableEntry {
+public:
+    /**
+     * The statement describing the memory object we're
+     * referincing in this mapping, for instance this could
+     * be a `malloc()` call.
+     */
+    const llvm::Value* Statement;
+
+    /**
+     * The nesting level of the loops containing @Statement
+     */
+    const int nestingLevel;
+
+    /**
+     * The rank of the ewpt mapping, that is, the amount of its
+     * parameters. For instance, in p(x1, x2), the rank would be
+     * 2.
+     */
+    const int rank;
+
+    std::vector<EWPTConstraint> constraints;
+    std::vector<EWPTIndexConstraint> indexConstraints;
+
+    int getSubscriptVariable(int index) const {
+        return index;
+    }
+
+    int getParameterVariable(int index) const {
+        return nestingLevel + index;
+    }
+
+    EWPTTableEntry(const llvm::Value* Statement, int nestingLevel, int rank)
+        : Statement(Statement), nestingLevel(nestingLevel), rank(rank) { }
+
+    void addConstraint(int variableIndex, EWPTIndexConstraint indexConstraint) {
+        indexConstraints.push_back(indexConstraint);
+        // indexConstraints.size() - 1 refers to the last inserted `b` index variable.
+        constraints.push_back(EWPTConstraint(variableIndex, indexConstraints.size() - 1));
+    }
 };
 
 class ElementWisePointsToMapping {
 public:
-    std::vector<EWPTMemoryRegion> mapping;
+    std::vector<EWPTTableEntry> tableEntries;
 };
 
 class EWPTAliasAnalysis: public FunctionPass {
@@ -48,7 +98,8 @@ public:
             if(llvm::isIdentifiedObject(&CurrentInstruction)) {
                 // If it's a malloc() call or similar.
                 ElementWisePointsToMapping *newMapping = new ElementWisePointsToMapping();
-                newMapping->mapping.push_back(EWPTMemoryRegion(CurrentInstruction));
+                EWPTTableEntry tableEntry(&CurrentInstruction, 0, 0);
+                newMapping->tableEntries.push_back(tableEntry);
                 ewpts[&CurrentInstruction] = newMapping;
                 llvm::outs() << "Found malloc instance: " << CurrentInstruction << "\n";
             }
